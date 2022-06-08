a8ab64ec4815a0ed646d729d55c6f496f3ee2cbc
architsaigal <97509105+architsaigal@users.noreply.github.com>
2022-06-08 21:53:49 +0530

    Update grocery.c

--- grocery.c
+++ grocery.c
@@ -16,3 +16,30 @@ void main()
 
     }
 }
+
+int add_item()
+{
+    FILE* fp = fopen("grocerylist.csv", "w");
+    int itemno, qty, price;
+    char itemname[50];
+    if (!fp)
+        //Error in opening file
+        printf("Cannot open file");
+        return 0;
+
+    else
+    {
+        printf("Enter item number: ");
+        scanf("%d", &itemno);
+        printf("Enter item name: ");
+        scanf("%s", &itemname);
+        printf("Enter quantity: ");
+        scanf("%d", &qty);
+        printf("Enter price: ");
+        scanf("%d", &price);
+        fprintf(fp, "%d, %s, %d, %d\n", itemno, itemname, qty, price);
+        printf("New item added successfully");
+    }
+
+    return 0;   
+}
