e24c2d2c4a74266a80448888f6301be168e94989
Anurag Vadhyar <92390229+anuragvadhyar@users.noreply.github.com>
2022-06-08 21:02:18 +0530

    Add files via upload
    main code

---
+++ grocery.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int priv;
+    char id[100],password[100];
+    printf("Select 1 if you are an admin or 2 if you are a buyer");
+    scanf("%d",&priv);
+    if (priv==1)
+    {
+        printf("Enter your username:");
+        scanf("%s",&id);
+        if (st)
+
+    }
+}
