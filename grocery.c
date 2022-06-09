ebb846527601b1da5aaadf1dd0d32aed2c16bac4
architsaigal <97509105+architsaigal@users.noreply.github.com>
2022-06-09 20:33:32 +0530

    Update grocery.c

--- grocery.c
+++ grocery.c
@@ -1,21 +1,5 @@
 #include<stdio.h>
 #include<string.h>
-void main()
-{
-    int priv;
-    char id[100],password[100];
-    printf("Select 1 if you are an admin or 2 if you are a buyer");
-    scanf("%d",&priv);
-    if (priv==1)
-    {
-        printf("Enter your username:");
-        scanf("%s",&id);
-        if (strcmp((id,"admin")==1)
-            {
-                printf("HI ARCHIT");
-
-    }
-}
 
 int add_item()
 {
